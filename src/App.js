940bb135eda302a1cf6d204f0f8f79fe866bcf6c
Nisarg <nisarg@aiminfologics.com>
2022-06-07 13:07:14 -0700

    lazy loading added to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import Layout from "./components/Layout/Layout";
 import { Route, Routes, Navigate, useLocation } from "react-router-dom";
-import Homepage from "./pages/Homepage";
 import NotFound from "./components/NotFound";
-import AboutUs from "./pages/AboutUs";
 import { ToastContainer } from "react-toastify";
 import {
   homePage,
@@ -16,20 +14,23 @@ import {
   categoryId,
   checkout,
 } from "./Routes/Routes";
-import Category from "./pages/Category";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
 import { tokenLogin } from "./store/Auth";
 import { useDispatch, useSelector } from "react-redux";
-import Cart from "./pages/Cart";
-import ProductDetail from "./pages/ProductDetail";
-import CheckOut from "./pages/CheckOut";
+
+const Homepage = React.lazy(() => import("./pages/Homepage"));
+const Login = React.lazy(() => import("./pages/Login"));
+const Cart = React.lazy(() => import("./pages/Cart"));
+const SignUp = React.lazy(() => import("./pages/SignUp"));
+const AboutUs = React.lazy(() => import("./pages/AboutUs"));
+const Category = React.lazy(() => import("./pages/Category"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const CheckOut = React.lazy(() => import("./pages/CheckOut"));
 
 function App() {
   const routePath = useLocation();
   const dispatch = useDispatch();
   const { isAuth } = useSelector((state) => state.auth);
+
   const onTop = () => {
     window.scrollTo(0, 0);
   };
@@ -46,25 +47,33 @@ function App() {
     <>
       <ToastContainer />
       <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate replace to={homePage} />} />
-          <Route
-            path={login}
-            element={
-              isAuth ? <Navigate replace to={`/${homePage}`} /> : <Login />
-            }
-          />
-          <Route path={cart} element={<Cart />} />
-          <Route path={signUp} element={<SignUp />} />
-          <Route path={homePage} element={<Homepage />} />
-          <Route path={aboutUs} element={<AboutUs />} />
-          <Route path={categoryId} element={<Category />} />
-          <Route path={category} element={<Category />} />
-          <Route path={contact} element={<Contact />} />
-          <Route path={checkout} element={<CheckOut />} />
-          {/* <Route path={"product"} element={<ProductDetail />} /> */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="centered">
+              Loading...
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Navigate replace to={homePage} />} />
+            <Route
+              path={login}
+              element={
+                isAuth ? <Navigate replace to={`/${homePage}`} /> : <Login />
+              }
+            />
+            <Route path={cart} element={<Cart />} />
+            <Route path={signUp} element={<SignUp />} />
+            <Route path={homePage} element={<Homepage />} />
+            <Route path={aboutUs} element={<AboutUs />} />
+            <Route path={categoryId} element={<Category />} />
+            <Route path={category} element={<Category />} />
+            <Route path={contact} element={<Contact />} />
+            <Route path={checkout} element={<CheckOut />} />
+            {/* <Route path={"product"} element={<ProductDetail />} /> */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </>
   );
